0a4720849db530ce4dfa77aca40409adf2a12301
Kevin Brockhoff <kbrockhoff@codekaizen.org>
2025-08-06 17:04:17 -0500

    initial dev

---
+++ src/test/java/org/codekaizen/demos/rewrite/UpgradeParentVersionTest.java
@@ -0,0 +1,35 @@
+package org.codekaizen.demos.rewrite;
+
+import org.junit.jupiter.api.Test;
+import org.openrewrite.test.RewriteTest;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Fail.fail;
+import static org.openrewrite.maven.Assertions.pomXml;
+
+public class UpgradeParentVersionTest implements RewriteTest {
+
+    @Test
+    void upgradesParentVersion() {
+        rewriteRun(
+          spec -> spec.recipe(getClass().getResourceAsStream("/META-INF/rewrite/framework-upgrade.yml"), "org.codekaizen.demos.rewrite.UpgradeParentVersion"),
+          pomXml(loadFile("parent-pom-upgrade-before.xml"),
+            loadFile("parent-pom-upgrade-after.xml"))
+        );
+    }
+
+    private String loadFile(String fileName) {
+        try {
+            Path path = Paths.get(getClass().getClassLoader().getResource(fileName).toURI());
+            return Files.readString(path);
+        } catch (URISyntaxException | IOException e) {
+            fail(e);
+            return "";
+        }
+    }
+}
